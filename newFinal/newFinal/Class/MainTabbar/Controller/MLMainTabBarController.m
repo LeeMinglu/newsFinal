d9906b441a09b6924967a925269eb3b9f0cef0e2
LeeMinglu <402007353@qq.com>
2015-12-08 21:40:51 +0800

    1. // 1. 初始化子控制器 - (void)setupChildViewControllers {  // 1. 新闻中心    HMNavigationController *navMainHall = [self navigationControllerWithStoryboardName:@"Main"];
         self.viewControllers = @[navMainHall, navReadingHall, navVedioHall, navDiscoverHall, navMeHall]; }
    
    2.- (HMNavigationController *)navigationControllerWithStoryboardName:(NSString *)name{ // 1. 加载storyboard文件    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:name bundle:nil];        // 2. 创建storyboard中箭头指向的初始化控制器, 也就是自定义的导航控制器    return [storyboard instantiateInitialViewController];}

--- newFinal/newFinal/Class/MainTabbar/Controller/MLMainTabBarController.m
+++ newFinal/newFinal/Class/MainTabbar/Controller/MLMainTabBarController.m
@@ -7,6 +7,14 @@
 //
 
 #import "MLMainTabBarController.h"
+#import "MLNavigationViewController.h"
+
+//tabbar子控制器
+#import "MLMainViewController.h"
+#import "MLReadingViewController.h"
+#import "MLDiscoverViewController.h"
+#import "MLVideoViewController.h"
+#import "MLMeViewController.h"
 
 @interface MLMainTabBarController ()
 
@@ -16,22 +24,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    //初始化子控制器
+    [self setupChildController];
+    
+    //初始化底部自定义tabbar
+    [self setupCustomTabBar];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/**
+ *  初始化子控制器
+ */
+- (void)setupChildController {
+    //1.新闻中心
+    MLNavigationViewController *navMainHall = [self navigationControllerWithStoryBoardName:@"Main"];
+    
+    
+    //2.阅读中心
+    MLNavigationViewController *navReadHall = [self navigationControllerWithStoryBoardName:@"Reading"];
+    
+    //3.发现
+    MLNavigationViewController *navDiscoverHall = [self navigationControllerWithStoryBoardName:@"Discover"];
+    
+    //4.视频中心
+    MLNavigationViewController *navVideoHall = [self navigationControllerWithStoryBoardName:@"Video"];
+    
+    //5.我
+    MLNavigationViewController *navMeHall = [self navigationControllerWithStoryBoardName:@"Me"];
+
+    //6.将上面的控制器添加到MLMainTabBarController中
+    self.viewControllers = @[navMainHall, navReadHall, navDiscoverHall, navVideoHall, navMeHall];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+/**
+ *  初始化自定义底部tabBar
+ */
+- (void)setupCustomTabBar {
+    
 }
-*/
+
+- (MLNavigationViewController *) navigationControllerWithStoryBoardName:(NSString *) storyBoardName {
+    
+    //1.加载storyBoard文件
+    UIStoryboard  *storyBoard = [UIStoryboard storyboardWithName:storyBoardName bundle:nil];
+    
+    //2.创建storyBoard中箭头指向的初始化服务器
+    return [storyBoard instantiateInitialViewController];
+
+}
 
 @end
